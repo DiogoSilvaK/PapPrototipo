91b945ac1e2f4434c891da7e6fb631fac3d35038
DiogoSilvaK <diogosilva190604@gmail.com>
2022-03-19 23:12:35 +0000

    19/03

--- PapPrototipo/Inserir.cs
+++ PapPrototipo/Inserir.cs
@@ -327,14 +327,14 @@ namespace PapPrototipo
                     break;
 
                 case "Lista de Peças":
-                    consultaSql1 = "SELECT * FROM Lista_de_pecas WHERE Cod_Servico=(SELECT Cod_Servico FROM )";
+                    consultaSql1 = "SELECT * FROM Lista_de_pecas WHERE Cod_Servico=(SELECT Cod_Servico FROM Servico where LoginEmail='"+ Login.UserLogado+"')";
                     SDR = "Deseja inserir a peça "+TBox1.Text+" para o serviço "+ CBox1.Text+"?";
                     consultaSql = "insert into Lista_de_pecas(Cod_Peca, Nome, Marca, Num_Serie, Preco, Cod_Servico, Desconto) VALUES(" + TBox1.Text + ",'" + TBox2.Text + "','" + TBox3.Text+ "','" + TBox6.Text + "','" + TBox4.Text + "','" + CBox1.Text + "', '"+ TBox5.Text+"')";
                     if (TBox1.Text == "" || TBox2.Text == "" || TBox3.Text == "" || TBox4.Text == "" || TBox6.Text == "" || CBox1.Text == "") aut = false;
                     break;
 
                 case "Veículos":
-                    consultaSql1 = "SELECT * FROM Veiculo";
+                    consultaSql1 = "SELECT * FROM Veiculo WHERE Matricula=(SELECT VeiculoMatricula FROM Servico WHERE LoginEmail='" + Login.UserLogado + "') ";
                     SDR = "Deseja inserir o veiculo "+TBox1.Text+" "+TBox2.Text+ " de matricula "+TBox3.Text+"?";
                     consultaSql = "insert into veiculo(Marca, Modelo, Matricula, Cilindrada, Mes_Ano, Cod_Cliente) VALUES('" + TBox1.Text + "','" + TBox2.Text + "','" + TBox3.Text + "'," + TBox5.Text + ",'" + MesAnoV.Text + "'," + CBox1.Text + ")";
                     if (TBox2.Text == "" || TBox1.Text == ""|| TBox3.Text == "" || TBox5.Text =="" || CBox1.Text == "") aut = false;
